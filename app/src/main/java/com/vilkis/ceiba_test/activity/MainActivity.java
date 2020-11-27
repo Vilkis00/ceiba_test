b1b37054f5056e8d906b7939c1dda5b58bc2bb6e
Vilkis00 <daniel.morales@mantum.com.co>
2020-11-27 08:57:24 -0500

    first commit

---
+++ app/src/main/java/com/vilkis/ceiba_test/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vilkis.ceiba_test.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vilkis.ceiba_test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
